80c4d3d781a14a40580683f1198375bb390c59d0
Marcio Hariki <marciohariki@gmail.com>
2019-10-04 05:03:18 -0300

    Updates

---
+++ precache-manifest.38f24b60e403df36722569f5968757ed.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d052a35c4d2e348dca9485c4cf1ad2bb",
+    "url": "/hal9000-performance-monitor/index.html"
+  },
+  {
+    "revision": "c75785493707dfa72c4f",
+    "url": "/hal9000-performance-monitor/static/js/2.c3b54b9a.chunk.js"
+  },
+  {
+    "revision": "61e44125cec6eef59fc6",
+    "url": "/hal9000-performance-monitor/static/js/main.5d7ca7d4.chunk.js"
+  },
+  {
+    "revision": "89a3f26216f4c0a6dad7",
+    "url": "/hal9000-performance-monitor/static/js/runtime~main.ddf60234.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26cb172bc7074ffc52d6d1ddeb23bdae.js"
+  "/hal9000-performance-monitor/precache-manifest.38f24b60e403df36722569f5968757ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hal9000-performance-monitor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
